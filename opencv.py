cbc3c88ba9852a9372c72027e4db18e43c3b994e
GPR8 <guilherme.polonia@gmail.com>
2019-12-26 17:52:35 +0000

    main_V2

--- opencv.py
+++
@@ -1,4 +0,0 @@
-import cv2 as cv
-
-# img = cv.imread('')
-print( cv.__version__ )
--- scritp_sap.py
+++
@@ -1,2 +0,0 @@
-from pyrfc import Connection
-conn = Connection(user='me', passwd='secret', ashost='10.0.0.1', sysnr='00', client='100')
